e82a3fe82457130a562dd8e11dd3afbb7fdbee1e
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-09-17 06:40:51 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Karanir",
+    version="1.0",
+    author="Yiqi Sun (Zongjing Li)",
+    author_email="ysun697@gatech.edu",
+    description="Melkor of Iron Crown, the personal package for machine learning and more",
+
+    # project main page
+    url="http://jiayuanm.com/", 
+
+    # the package that are prerequisites
+    packages=find_packages(),
+    include_package_data = True,
+    package_data={
+        },
+    
+)
+
+"""
+'':['moic',
+        'moic/mklearn',
+        'moic/learn/nn'],
+        'moic': ['mklearn'],
+        'bandwidth_reporter':['moic','moic/mklearn','moic/learn/nn']
+               
+"""

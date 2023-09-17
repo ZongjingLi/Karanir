8a357489e7853823d5afd5addb58f51ad9b80f54
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-09-17 17:23:51 +0800

    add some algorithm utils

--- Karanir/__init__.py
+++ Karanir/__init__.py
@@ -2,5 +2,6 @@ from .dklearn import *
 from .utils import *
 from .math import *
 from .physics import *
+from .algs import *
 #print("A Karanir Thanagor")
 from .import *
---
+++ Karanir/algs/graph.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+class Graph:
+    def __init__(self,nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+    
+    def bellman_ford(self, start):
+        return 0
+
+    def astar_search(self,goal,heurstics):
+        return 
